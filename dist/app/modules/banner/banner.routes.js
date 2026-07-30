a3d411c32e90bae7f711c5c87f1f8ddca35b78f3
MHkhan994 <khanmahmud994@gmail.com>
2026-07-30 23:21:53 +0600

    chore: update bcrypt dependency to version 6.0.0

--- dist/app/modules/banner/banner.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BannerRoutes = void 0;
-const express_1 = require("express");
-const banner_controller_1 = require("./banner.controller");
-const router = (0, express_1.Router)();
-router.get("/get-banner", banner_controller_1.BannerController.getBanner);
-exports.BannerRoutes = router;
